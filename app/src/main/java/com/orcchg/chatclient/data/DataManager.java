33d906b3cb8580d1c8480e4e7be858bf0cbd956b
Alov Maxim <alovmax@yandex.ru>
2016-05-02 17:33:18 +0300

    Organized sources

---
+++ app/src/main/java/com/orcchg/chatclient/data/DataManager.java
@@ -0,0 +1,7 @@
+package com.orcchg.chatclient.data;
+
+/**
+ * Created by MAXA on 02.05.2016.
+ */
+public class DataManager {
+}
--- app/src/main/java/com/orcchg/chatclient/ui/LoginActivity.java
+++ app/src/main/java/com/orcchg/chatclient/ui/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.orcchg.chatclient;
+package com.orcchg.chatclient.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.orcchg.chatclient.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
