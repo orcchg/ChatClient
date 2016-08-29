d78b15eb6d2d05a8688e167ecd9a87b3b266822a
Maxim Alov <orcchg@yandex.ru>
2016-08-29 19:32:46 +0300

    Upload script added for privacy policy file and server

---
+++ privacy_policy/build_and_upload.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 pp_server.cpp -o chatclient_privacy_policy_server && scp privacy_policy.html chatclient_privacy_policy_server maxa@194.190.63.108:/home/maxa/Develop/Server/ChatServer
+
